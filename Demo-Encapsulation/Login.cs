512e68b1dc64ec1a03c56fd09b07c44bbbab109d
Legrain Samuel <samuel.legrain@bstorm.be>
2024-05-13 11:16:19 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-Encapsulation/Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Encapsulation
+{
+    internal class Login
+    {
+        private string _username;
+
+        public string Password { private get; set; }
+
+        public string Username
+        {
+            private get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value.Trim();
+                if (_username == "Admin") Password = "Admin";
+            }
+        }
+
+        public bool CheckUser(string user_name, string pwd)
+        {
+            return user_name == _username && Password == pwd;
+        }
+    }
+}
---
+++ Demo-Encapsulation/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Demo_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countTry = 0;
+            Login log1 = new Login();
+            //log1.username = "SamBStorm";
+            log1.Username = "   Admin ";
+            //log1.Password = "password";
+
+            Console.WriteLine($"Le login est sauvegardé!");
+
+            Console.WriteLine("Veuillez vous identifier :");
+            string user_username = Console.ReadLine();
+            Console.WriteLine("Veuillez introduire votre mot de passe :");
+            string user_pwd = Console.ReadLine();
+            countTry++;
+            while (!log1.CheckUser(user_username, user_pwd) && countTry < 3)
+            {
+                Console.WriteLine("Veuillez réessayer ...");
+                Console.WriteLine("Veuillez vous identifier :");
+                user_username = Console.ReadLine();
+                Console.WriteLine("Veuillez introduire votre mot de passe :");
+                user_pwd = Console.ReadLine();
+                countTry++;
+            }
+            if(log1.CheckUser(user_username, user_pwd))
+            {
+                Console.WriteLine($"Bonjour {user_username}");
+            }
+            else
+            {
+                Console.WriteLine("Votre compte est bloqué");
+            }
+        }
+    }
+}
