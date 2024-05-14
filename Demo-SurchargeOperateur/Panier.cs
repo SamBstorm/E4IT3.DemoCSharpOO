802e953399a02ce3d16bf40d948ec680d7f2c52f
Legrain Samuel <samuel.legrain@bstorm.be>
2024-05-14 12:30:37 +0200

    Ajout Demo-SurchargeOperateur

---
+++ Demo-SurchargeOperateur/Panier.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_SurchargeOperateur
+{
+    internal class Panier
+    {
+        private List<Fruit> _fruits = new List<Fruit>();
+
+        public int Count
+        {
+            get { return _fruits.Count; }
+        }
+
+        public void AjouterFruit(Fruit fruit)
+        {
+            _fruits.Add(fruit);
+        }
+
+        public static Panier operator + (Panier left, Panier right)
+        {
+            Panier result = new Panier();
+
+            foreach (Fruit f in left._fruits)
+            {
+                result.AjouterFruit(f);
+            }
+
+            foreach (Fruit f in right._fruits)
+            {
+                result.AjouterFruit(f);
+            }
+
+            return result;
+        }
+
+        public static Panier operator + (Panier left, Fruit right)
+        {
+            left.AjouterFruit(right);
+            return left;
+        }
+    }
+}
---
+++ Demo-SurchargeOperateur/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Demo_SurchargeOperateur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit pomme01 = new Fruit();
+            Fruit pomme02 = new Fruit();
+            Fruit pomme03 = new Fruit();
+
+            Panier panier01 = new Panier();
+            panier01.AjouterFruit(pomme01);
+            panier01.AjouterFruit(pomme02);
+            panier01.AjouterFruit(pomme03);
+            Console.WriteLine($"Le premier panier contient {panier01.Count} fruit(s).");
+
+            Fruit orange01 = new Fruit();
+            Fruit orange02 = new Fruit();
+
+            Panier panier02 = new Panier();
+            panier02.AjouterFruit(orange01);
+            panier02.AjouterFruit(orange02);
+            Console.WriteLine($"Le second panier contient {panier02.Count} fruit(s).");
+
+            Panier recolte = panier01 + panier02;
+
+            Console.WriteLine($"La récolte total contient {recolte.Count} fruit(s).");
+
+            recolte = recolte + new Fruit();
+            Console.WriteLine($"La récolte total contient {recolte.Count} fruit(s).");
+        }
+    }
+}
